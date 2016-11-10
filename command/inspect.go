ae4b329a674037f0905c175093d3142f6a167099
timakin <timaki.st@gmail.com>
2016-11-11 00:34:57 +0900

    [add] create tmp dir

--- command/inspect.go
+++ command/inspect.go
@@ -1,8 +1,22 @@
 package command
 
-import "github.com/codegangsta/cli"
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+	"os"
+	"path/filepath"
+)
+
+var tmpPath = []string{"/tmp", "pppb_tmp"}
 
 func CmdInspect(c *cli.Context) {
-	// Write your code here
+	createTmpDir()
+}
 
+func createTmpDir() {
+	dir := filepath.Join(tmpPath...)
+
+	if err := os.Mkdir(dir, 0777); err != nil {
+		fmt.Println(err)
+	}
 }
